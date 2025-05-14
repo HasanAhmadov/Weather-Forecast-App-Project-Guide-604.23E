0da2f0d231a4a9b9270a34fb2d28367f5425e271
HasanAhmadov <prohesen@gmail.com>
2025-05-14 11:15:19 +0400

    Hasan loaded dependencies and API

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+/*Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(MyApp());
+}*/
 
 void main() {
   runApp(const MyApp());
